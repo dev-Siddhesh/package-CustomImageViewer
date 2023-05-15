1bb974ba62b1fa683f446779f15e256e11cc14de
Siddhesh Jadhav <siddhesh.jadhav@sportzinteractive.net>
2023-05-15 18:28:13 +0530

    Added Demo Class

---
+++ Sources/CustomImageViewer/Demo.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Siddhesh Jadhav on 15/05/23.
+//
+
+import Foundation
+
+public class Demo {
+    let a = "A"
+    var b = ""
+    
+    public func returnB() -> String {
+        b = "B"
+        return b
+    }
+}
